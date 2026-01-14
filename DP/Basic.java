3bfdad6aa783e3cb76297ad7ec08af327913248e
Rajan Tripathi <rajantripathi123@gmail.com>
2026-01-14 21:12:20 +0530

    Initial commit - Java DSA codes

--- DP/Basic.java
+++ DP/Basic.java
@@ -12,6 +12,16 @@ public class Basic {
       return f[n];
     }
 
+    public static int fibTubulation(int n) {
+      int f[] = new int[n+1];
+      f[0] = 0;
+      f[1] = 1;
+      for (int i = 2; i <= n; i++) {
+        f[i] = f[i-1] + f[i-2];
+      }
+      return f[n];
+    }
+
     //fibnaci
     public static void main(String[] args) {
         int n=5;
